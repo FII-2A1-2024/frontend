84641caa76bb54ac7a1648f37666e4930242d858
Patricia Onisor <77615799+AuraBluestar@users.noreply.github.com>
2024-03-29 17:02:05 +0200

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,6 @@
+import Message from './Message';
+
+function App(){
+  return <div><Message /></div>;
+}
+export default App;
---
+++ src/Message.tsx
@@ -0,0 +1,10 @@
+
+// PascalCase
+function Message(){
+    //JSX: JavaScrip XML
+    const name='Proiectul la ip';
+    if(name)
+        return <h1>Bun gasit! Vizionati {name}</h1>;
+    else return <h1>Hello World!</h1>;
+}
+export default Message;
