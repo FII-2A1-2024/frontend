461c2982618614b6e9c6862acc909b95d7bb638e
Patricia Onisor <77615799+AuraBluestar@users.noreply.github.com>
2024-04-06 21:27:47 +0300

    Update LandingPage.jsx

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,14 +1,16 @@
 import { Link } from "react-router-dom";
+import Navbar from './Navbar';
 
 function LandingPage() {
     const name = 'Ugly Button';
-    return (<>
+    return (<>       
         <h1>{name}</h1>
         <p>Pagina principala (main)</p>
+        <div><Navbar /></div>
         <div className="flex flex-col gap-1 temporary">
             <Link to="/connect">Conectare</Link>
             <Link to="/conversation">Conversatii</Link>
-        </div>
+        </div>  
     </>);
 }
 
