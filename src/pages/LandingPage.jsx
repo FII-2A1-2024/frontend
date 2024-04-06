942901bf2dfdda1256b266429d6a7b6374583a9c
Patricia Onisor <77615799+AuraBluestar@users.noreply.github.com>
2024-04-06 23:32:52 +0300

    Update LandingPage.jsx

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -3,10 +3,10 @@ import Navbar from '../components/Navbar';
 
 function LandingPage() {
     const name = 'Ugly Button';
-    return (<>       
+    return (<>      
         <h1>{name}</h1>
         <p>Pagina principala (main)</p>
-        <div><Navbar /></div>
+        <Navbar />
         <div className="flex flex-col gap-1 temporary">
             <Link to="/connect">Conectare</Link>
             <Link to="/conversation">Conversatii</Link>
