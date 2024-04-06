cb91a69c5667e9fefb8dc3f68b4b442d8ecb9c73
Patricia Onisor <77615799+AuraBluestar@users.noreply.github.com>
2024-04-06 21:23:48 +0300

    Add files via upload

---
+++ LandingPage.jsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom";
+import Navbar from './Navbar';
+
+function LandingPage() {
+    const name = 'Ugly Button';
+    return (<>
+        <div>       
+            <h1>{name}</h1>
+            <p>Pagina principala (main)</p>
+            <div><Navbar /></div>
+            <div className="flex flex-col gap-1 temporary">
+                <Link to="/connect">Conectare</Link>
+                <Link to="/conversation">Conversatii</Link>
+            </div>
+        </div>   
+    </>);
+}
+
+export default LandingPage
