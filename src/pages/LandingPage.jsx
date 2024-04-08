a1ef37dc15638c037727b5734e0f7042721a4cde
daniela.rusu <rusudaniela2003@gmail.com>
2024-04-09 01:07:20 +0300

    Adding the post-template unfinished

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,10 +1,12 @@
 import { Link } from "react-router-dom";
+import Post from '../components/post';
 
 function LandingPage() {
     const name = 'Ugly Button';
     return (<>
         <h1>{name}</h1>
         <p>Pagina principala (main)</p>
+        < Post userName='Samuel Jackson' title='The title goes here' content='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut ' upVotesCount={124} commentsCount={12}/>
         <div className="flex flex-col gap-1 temporary">
             <Link to="/connect">Conectare</Link>
             <Link to="/conversation">Conversatii</Link>
