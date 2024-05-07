9d6a7eee60a841efddc643d2b662a4fa02c34313
AuraBluestar <onisor.patricia@yahoo.com>
2024-05-07 22:55:29 +0300

    Aranjat in fisiere

--- src/components/SideBarChats/SideBarChats.jsx
+++ src/components/SideBarChats/SideBarChats.jsx
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
-import SideBarChat from "../SideBarChat/SideBarChat"
-import "./SideBarChats.css"; 
+import SideBarChat from "./SideBarChat/SideBarChat";
+import "./SideBarChats.css";
 
 function SideBarChats() {
   const [messages, getAll] = useState([]);
 
-{/*useEffect(() => {
+  {
+    /*useEffect(() => {
     axios
       .get("http://localhost:3000/messages/all")
       .then((response) => {
@@ -16,23 +17,54 @@ function SideBarChats() {
       .catch((error) => {
         console.error("Error fetching posts:", error);
       });
-  }, []);*/}
+  }, []);*/
+  }
 
   return (
     <div className="side_bar_chats_body">
-     <h1>Chats</h1>
-     {/*  {messages.map((message) => (
+      <h1>Chats</h1>
+      {/*  {messages.map((message) => (
           <SideBarChat
             key={post.id}
             username={`User ${message.author_id}`}
             content={message.description}
           />
         ))}*/}
-        <Link to="/"><SideBarChat id={0} username={"User1"} content={"LastMessageLastMessageLastMessageLastM"} /></Link>
-        <Link to="/"><SideBarChat id={0} username={"User2"} content={"LastMessageLastMessageLastMessageLastM"} /></Link>
-        <Link to="/"><SideBarChat id={0} username={"User3"} content={"LastMessageLastMessageLastMessageLastM"} /></Link>
-        <Link to="/"><SideBarChat id={0} username={"User4"} content={"LastMessageLastMessageLastMessageLastM"} /></Link>
-        <Link to="/"><SideBarChat id={0} username={"User5"} content={"LastMessageLastMessageLastMessageLastM"} /></Link>
+      <Link to="/">
+        <SideBarChat
+          id={0}
+          username={"User1"}
+          content={"LastMessageLastMessageLastMessageLastM"}
+        />
+      </Link>
+      <Link to="/">
+        <SideBarChat
+          id={0}
+          username={"User2"}
+          content={"LastMessageLastMessageLastMessageLastM"}
+        />
+      </Link>
+      <Link to="/">
+        <SideBarChat
+          id={0}
+          username={"User3"}
+          content={"LastMessageLastMessageLastMessageLastM"}
+        />
+      </Link>
+      <Link to="/">
+        <SideBarChat
+          id={0}
+          username={"User4"}
+          content={"LastMessageLastMessageLastMessageLastM"}
+        />
+      </Link>
+      <Link to="/">
+        <SideBarChat
+          id={0}
+          username={"User5"}
+          content={"LastMessageLastMessageLastMessageLastM"}
+        />
+      </Link>
     </div>
   );
 }
