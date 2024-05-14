b1cab8f2ec95eda2283c0c65f946b4a28aba1836
Gintu Ciprian <ciprian.florin19g@gmail.com>
2024-05-14 18:06:43 +0300

    fix: CSS styling

--- src/components/SideNavbar/Navbar.jsx
+++ src/components/SideNavbar/Navbar.jsx
@@ -153,7 +153,7 @@ function Navbar() {
 
         <div className={`side-bar-item ${activeItem === 'rules' ? 'active' : ''}`} onClick={() => handleItemClick('rules')}>
           <img src={activeItem === 'rules' ? rulesIconWhite : rulesIconRed} alt="rulesIcon" className="side-bar-item-icon" />
-          <Link to='/' className="navbar-link">Rules</Link>
+          <Link to="/rules" className="navbar-link">Rules</Link>
         </div>
 
         {/* <Link to='/' className="navbar-link" isActive={(match, location) => location.pathname === '/'}>
