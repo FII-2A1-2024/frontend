397d4ed5419a119dd119400aeda9f7b439b3f4b4
Tudor Gradinaru <126348970+TudorGR@users.noreply.github.com>
2024-05-14 20:08:21 +0300

    remove temp button

--- src/components/AuthButton.jsx
+++ src/components/AuthButton.jsx
@@ -7,26 +7,10 @@ import { useState } from "react";
 function AuthButton() {
   const {
     t,
-    i18n: { changeLanguage, language },
   } = useTranslation();
-  const [currentLanguage, setCurrentLanguage] = useState(language);
-  const handleChangeLanguage = () => {
-    const newLanguage = currentLanguage == "en" ? "ro" : "en";
-    setCurrentLanguage(newLanguage);
-    changeLanguage(newLanguage);
-    localStorage.setItem("language", newLanguage);
-  };
-
-  //
 
   return (
     <div className="auth-buttons">
-      <button
-        onClick={handleChangeLanguage}
-        className="absolute left-[-500px] text-black"
-      >
-        schimba limba
-      </button>
       <Link to="/">{t("loginButton")}</Link>
     </div>
   );
