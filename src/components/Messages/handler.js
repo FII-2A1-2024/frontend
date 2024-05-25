6867983ae7c9eb1a7a61abe5a06b8f956f69fe2f
Gintu Ciprian <ciprian.florin19g@gmail.com>
2024-05-25 17:08:21 +0300

    feat: side-chat container

--- src/components/Messages/handler.js
+++ src/components/Messages/handler.js
@@ -7,7 +7,6 @@ export const sendMessage = async (sender_id, receiver_id, content) => {
             receiver_id: receiver_id,
             content: content
         });
-        console.log("Message sent response:", response.data);
         return response.data;
     } catch (error) {
         console.error("Error sending the message", error);
