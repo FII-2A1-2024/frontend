380373120c4801fbca9400af5578f90a8c825523
Tudor Gradinaru <126348970+TudorGR@users.noreply.github.com>
2024-05-25 16:47:51 +0300

    UserPostList

--- src/pages/UserAccountPage.jsx
+++ src/pages/UserAccountPage.jsx
@@ -1,23 +1,27 @@
-import React from 'react';
-import Navbar_superior from '../components/Navbar_Superior/Navbar_superior';
-import Navbar from '../components/SideNavbar/Navbar';
-import AccountInfo from '../components/UserAccount/AccountInfo';
-import '../components/UserAccount/Info.css';
+import React from "react";
+import Navbar_superior from "../components/Navbar_Superior/Navbar_superior";
+import Navbar from "../components/SideNavbar/Navbar";
+import AccountInfo from "../components/UserAccount/AccountInfo";
+import "../components/UserAccount/Info.css";
+import UserPostList from "../components/LandingPageComponents/PostsList/UserPostList";
 
 function UserAccountPage() {
-    return (
-        <div className="user-account-page">
-            <Navbar_superior />
-            <div className="user-account-page-content">
-              <div className="user-account-sidebar">
-                <Navbar />
-              </div>
-              <div className="user-account-info">
-                <AccountInfo />
-              </div>
-            </div>
+  return (
+    <div className="user-account-page">
+      <Navbar_superior />
+      <div className="user-account-page-content">
+        <div className="user-account-sidebar">
+          <Navbar />
         </div>
-    );
+        <div className="content-wrapper">
+          <div className="user-account-info">
+            <AccountInfo />
+          </div>
+          <div className="user-posts">{<UserPostList />}</div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default UserAccountPage;
