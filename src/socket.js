d5738b271077e04acd2e442f3baf4e0c5b8ff8e8
Gintu Ciprian <ciprian.florin19g@gmail.com>
2024-05-28 19:09:53 +0300

    feat: redirect to the same user chat even if the names differ

--- src/socket.js
+++ src/socket.js
@@ -1,15 +1,19 @@
 import io from 'socket.io-client';
 
-// const socket = io(`${import.meta.env.VITE_URL_BACKEND}`, 
-//     {
-//         rejectUnauthorized: false
-//     }
-// );
-
-const socket = io(`${import.meta.env.VITE_URL_BACKEND}`);
+const socket = io(import.meta.env.VITE_URL_SITE, {
+    path: import.meta.env.VITE_SOCKET_PATH,
+});
 
 socket.on('connect', () => {
     console.log('Connected to server, Socket ID:', socket.id);
+});
+
+socket.on('connect_error', (err) => {
+    console.error('Connection error:', err);
+});
+
+socket.on('disconnect', (reason) => {
+    console.log('Disconnected:', reason);
 });
 
 export default socket;
